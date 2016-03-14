cc066d84e51439702da4689bc9266e7b62e7b305
Kit Langton <kitlangton@gmail.com>
2016-03-14 12:20:40 -0400

    finished

--- groceries/groceries.js
+++ groceries/groceries.js
@@ -1,7 +1,27 @@
 // Your Code Here!
 
-$(function(){
+var addItem = function(name){
+  if(!name) {
+    return;
+  }
+  var newItem = $('<div>');
+  newItem.addClass('item');
+  var deleteBtn = $('<span>');
+  deleteBtn.text(' Delete');
+  deleteBtn.addClass(' delete');
+  newItem.text(name);
+  newItem.append(deleteBtn);
+  deleteBtn.click(function() {
+    newItem.fadeOut();
+  })
+  $('#list').append(newItem);
+}
+
+$(document).ready(function(){
   $('#new-item').submit(function(e){
+    var name = $('#item-name').val();
+    $('#item-name').val("");
+    addItem(name);
     e.preventDefault();
   })
 });
